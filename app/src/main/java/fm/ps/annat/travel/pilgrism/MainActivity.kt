c1dc85a78cced0ad1b71d1b11511c0be6f386090
Moath Raed Abu Ouda <85387360+Fmoath@users.noreply.github.com>
2022-02-17 11:08:31 +0200

    Initilize Project

---
+++ app/src/main/java/fm/ps/annat/travel/pilgrism/MainActivity.kt
@@ -0,0 +1,11 @@
+package fm.ps.annat.travel.pilgrism
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
