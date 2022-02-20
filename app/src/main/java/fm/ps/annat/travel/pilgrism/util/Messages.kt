39c05af3ee304a0d03e31543c88ac2f17862cf0d
Moath Raed Abu Ouda <85387360+Fmoath@users.noreply.github.com>
2022-02-20 03:55:51 +0200

    Create Validator -> Incorrect & Empty Validator

---
+++ app/src/main/java/fm/ps/annat/travel/pilgrism/util/Messages.kt
@@ -0,0 +1,19 @@
+package fm.ps.annat.travel.pilgrism.util
+
+import fm.ps.annat.travel.pilgrism.R
+
+/**
+ * Created With Love For : Annat Tarvel
+ * The owner has been granted all rights in the application and has the freedom to dispose of everything
+ * @by : Eng-Moath Raed Abu Ouda
+ * @CompanyManager : FM-Team
+ * @In : Palestine - Gaza - Biet Hanoun
+ * @OnDate : 2/20/2022
+ * @InTheHour : 03 : 53 : 01
+ * @Website : fm-ps.online
+ * @Whatsapp : 00972597718418
+ */
+object Messages {
+    const val ERROR_MESSAGE_EMPTY : Int = R.string.error_empty
+    const val ERROR_MESSAGE_INCORRECT : Int = R.string.error_incorrect
+}
---
+++ app/src/main/java/fm/ps/annat/travel/pilgrism/validator/EmptyValidator.kt
@@ -0,0 +1,34 @@
+package fm.ps.annat.travel.pilgrism.validator
+
+import android.widget.EditText
+import fm.ps.annat.travel.pilgrism.abstract.ValidatorAbstract
+import fm.ps.annat.travel.pilgrism.util.Messages
+
+/**
+ * Created With Love For : Annat Travel
+ * The owner has been granted all rights in the application and has the freedom to dispose of everything
+ * @by : Eng-Moath Raed Abu Ouda
+ * @CompanyManager : FM-Team
+ * @In : Palestine - Gaza - Biet Hanoun
+ * @OnDate : 2/20/2022
+ * @InTheHour : 03 : 52 : 34
+ * @Website : fm-ps.online
+ * @Whatsapp : 00972597718418
+ */
+class EmptyValidator(private var editText: EditText) : ValidatorAbstract(editText) {
+
+    override val isValid: Boolean
+        get() {
+            if (ViewsUtil.isTheEditTextEmpty(editText)) {
+                editText.error = getErrorMessage(editText)
+                return false
+            }
+            editText.error = null
+            return true
+        }
+
+    private fun getErrorMessage(editText: EditText): String {
+        return editText.context.getString(Messages.ERROR_MESSAGE_EMPTY, editText.hint)
+    }
+
+}
